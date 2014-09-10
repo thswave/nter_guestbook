7f1d4c558c99f6629e6e9215e8f0785405e60692
changwon <thswave@gmail.com>
2014-09-10 20:04:59 +0900

    #4 cubrid data insert test

---
+++ src/main/java/com/nhnent/basecamp/dao/GuestBookDao.java
@@ -0,0 +1,18 @@
+package com.nhnent.basecamp.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nhnent.basecamp.vo.GuestBook;
+
+@Repository
+public class GuestBookDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public int insertGuestBook(GuestBook guestBook) {
+		return sqlSession.insert("GuestBook.insertGuestBook");
+	}
+
+}
---
+++ src/test/java/com/nhnent/basecamp/dao/GuestBookDaoTest.java
@@ -0,0 +1,43 @@
+package com.nhnent.basecamp.dao;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.nhnent.basecamp.vo.GuestBook;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/test-servlet-context.xml",
+	     "file:src/main/webapp/WEB-INF/spring/mybatis/test-mybatis-context.xml"})
+public class GuestBookDaoTest {
+	@Autowired
+	private GuestBookDao guestBookDao;
+	private GuestBook guestBook;
+	
+	@Before
+	public void setUp(){
+		guestBook = new GuestBook();
+		Date date = new Date();
+		guestBook.setGuestBookId(1);
+		guestBook.setTitle("title");
+		guestBook.setContent("한글");
+		guestBook.setCreatedAt(date);
+		guestBook.setUpdatedAt(date);
+	}
+	
+	@Test
+	public void test() {
+		int insertResult = guestBookDao.insertGuestBook(guestBook);
+		assertThat(insertResult, is(equalTo(1)));
+	}
+
+}
